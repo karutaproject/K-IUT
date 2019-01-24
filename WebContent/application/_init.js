9c1d6a5bd12e46e4652fbcbf8fc28eb8bf98ec8e
olivier.gerbe <olivier.gerbe@hec.ca>
2019-01-24 13:14:05 -0500

    FTLV

---
+++ WebContent/application/_init.js
@@ -0,0 +1,35 @@
+//----------------------------------
+var application_version = "3.0";
+var application_date = "2018-09-11";
+//----------------------------------
+var appliname = 'ftlv';
+var applitype = 'FTLV';   // FTLV ou KIUT
+//---------
+var bckname = '-ftlv';
+var serverBCK = "karuta-backend"+bckname+"/rest/api";
+var serverFIL = "karuta-backend"+bckname;
+var serverVER = "karuta-backend"+bckname;
+//----ELGG-----
+var elgg_installed = false;
+var elgg_url_base = '';
+var elgg_url_absolute = '';
+var elgg_auth_cas = false;
+var g_elgg_refreshing = 120000; // 120s 
+//----------------------------------
+var languages = [];
+var languages_name = [];
+languages [0] = 'fr';
+languages_name ['fr'] = 'Français';
+languages [1] = 'en';
+languages_name ['en'] = 'English';
+//----------------------------------
+var NONMULTILANGCODE = 0;  // default language if non-multilingual
+var LANGCODE = 0; //default value
+var LANG = languages[LANGCODE]; //default value
+//----------------------------------
+var carte_metiers_url =  "https://iut2.univ-grenoble-alpes.fr/metiers/";
+var freerome = true;
+var freerome_url = "http://freerome.mugeco.com/plugin?uid=";
+var cas_url = "https://cas-uga.grenet.fr";
+var quizz_interculturel = true;
+var message_logo = "/application/img/logo-eportfolio4.jpg";
